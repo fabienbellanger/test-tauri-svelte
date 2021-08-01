32c50a304ea80366fb93a911c32dc73a82bb0beb
Fabien Bellanger <valentil@gmail.com>
2021-08-01 23:19:16 +0200

    Initial message

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// With the Tauri API npm package:
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	// Invoke the command
+	invoke('my_custom_command', { invokeMessage: 'Hello!' })
+		.then((message) => console.log(message));
+	let name: string = 'Fabien';
+</script>
+
+<style>
+	.name {
+		text-transform: capitalize;
+	}
+</style>
+
+<h1 class="name">Hello {name.toLowerCase()}!</h1>
